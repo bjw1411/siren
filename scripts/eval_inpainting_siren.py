416fff23743419f043634a0000f7d84a608e01db
bjw1411 <bjw1411@rit.edu>
2024-04-29 16:44:01 -0400

    Fixed documentation and cleaned up code.

--- scripts/eval_inpainting_siren.py
+++ scripts/eval_inpainting_siren.py
@@ -1,3 +1,6 @@
+# Originally forked from https://github.com/dalmia/siren
+# Authors: Aman Dalmia, Ben Williamson
+# Evaluates a trained SIREN model, to produce an image.
 import os
 import glob
 import cv2
@@ -9,18 +12,22 @@ import torch
 from torch.utils.data import TensorDataset, DataLoader
 from siren import SIREN
 
+# Batch Size Hyper Parameter
 BATCH_SIZE = 8192
 
+# Filename to read
 filename = 'bread.png'
 
+# Read the image in with PyTorch
 img_filepath = 'data/' + filename
 img_raw = np.array(Image.open(img_filepath))
 img_ground_truth = torch.from_numpy(img_raw).float()
 
+# Get the information from the image
 rows, cols, channels = img_ground_truth.shape
 pixel_count = rows * cols
 
-
+# Build tensores
 def build_eval_tensors():
     img_mask_x = np.arange(0, rows)
     img_mask_y = np.arange(0, cols)
@@ -34,13 +41,9 @@ def build_eval_tensors():
 
     img_mask = torch.stack([img_mask_x, img_mask_y], dim=-1)
     img_mask = img_mask.reshape(-1, 2)
-    #changed to -1 4 from -1 3
     img_eval = img_ground_truth.reshape(-1, 4)
 
-    #img_mask.to(device="cuda")
-    #img_eval.to(device="cuda")
     return img_mask, img_eval
-
 
 img_mask, img_eval = build_eval_tensors()
 
@@ -48,11 +51,12 @@ test_dataset = TensorDataset(img_mask, img_eval)
 test_dataloader = DataLoader(
     test_dataset, batch_size=BATCH_SIZE, shuffle=False, pin_memory=True)
 
-# Build model
+# Using SIREN build the model
+# model parameters
 layers = [256, 256, 256, 256, 256]
 in_features = 2
-# changed from 3 to 4
 out_features = 4
+# initialize with default SIREN initializer
 initializer = 'siren'
 w0 = 1.0
 w0_initial = 30.0
@@ -61,13 +65,15 @@ model = SIREN(
     layers, in_features, out_features, w0, w0_initial,
     initializer=initializer, c=c)
 
-# Restore model
+
+# Load the model from the checkpoints dir (last generated model)
+#Check checkpoint dir
 checkpoint_path = 'checkpoints/siren/inpainting/model'
 if not os.path.exists(checkpoint_path):
     raise FileNotFoundError('Checkpoint not found at {}'.format(
         checkpoint_path))
 
-# load checkpoint
+# Load model
 print('Loading model checkpoint from {}'.format(checkpoint_path))
 ckpt = torch.load(checkpoint_path)
 model.load_state_dict(ckpt['network'])
@@ -77,6 +83,7 @@ iterator = tqdm(test_dataloader)
 
 predictions = []
 
+# Predict for each batch
 for batch in iterator:
     inputs, _ = batch
 
@@ -85,25 +92,30 @@ for batch in iterator:
 
     predictions.append(prediction)
 
+# Convert values to image data
 predicted_image = torch.cat(predictions).cpu().numpy()
 predicted_image = predicted_image.reshape((rows, cols, channels)) / 255
 predicted_image = predicted_image.clip(0.0, 1.0)
 
+# Prepare image data for PIL image saving
 temp_array = predicted_image
 temp_array = temp_array * 255
 temp_array = temp_array.astype(np.uint8)
 
+# Save the image separately to its own file
 im = Image.fromarray(temp_array)
 im.save('images/Predicted ' + filename + ".png")
 
-img_save_path = 'images/' + filename + " compare.png"
-os.makedirs(os.path.dirname(img_save_path), exist_ok=True)
-
+# Load the ground truth, and predicted with cv2 then calculate PSNR
 ground_truth = cv2.imread(img_filepath)
 output = cv2.imread('images/Predicted ' + filename + ".png")
 psnr = cv2.PSNR(ground_truth, output)
 
 print(f"Ground Truth vs Predicted PSNR: {psnr}")
+
+# Save the comparison image to its file
+img_save_path = 'images/' + filename + " compare.png"
+os.makedirs(os.path.dirname(img_save_path), exist_ok=True)
 
 fig, axes = plt.subplots(1, 2)
 plt.sca(axes[0])
--- scripts/train_inpainting_siren.py
+++ scripts/train_inpainting_siren.py
@@ -1,3 +1,6 @@
+# Originally forked from https://github.com/dalmia/siren
+# Authors: Aman Dalmia, Ben Williamson
+# Trains SIREN model for inpainting task on a single image. Implements learning rate decay.
 import os
 from datetime import datetime, timedelta
 from PIL import Image
@@ -11,30 +14,36 @@ from torch.utils.data import TensorDataset, DataLoader
 from siren import SIREN
 from utils import set_logger
 
-#Model Hyper parameters
-#For the experiment we are keeping these constant
+# Model Hyper parameters
+# For the experiment we are keeping these constant
 SAMPLING_RATIO = 0.1
 BATCH_SIZE = 8192
 EPOCHS = 5000
-LEARNING_RATE = 0.0005
 
+# Initial learning rate
+LEARNING_RATE = 0.0015
+# Learning Rate decay from exponential
+decay_rate = 0.9997
+
+# Image to train on
 filename = 'bread.png'
 
-# Image Reference - http://earthsongtiles.com/celtic_tiles.html
+# Open the image with torch
 img_filepath = 'data/' + filename
 img_raw = np.array(Image.open(img_filepath))
 img_ground_truth = torch.from_numpy(img_raw).float()
 
+# Get values from the image
 rows, cols, channels = img_ground_truth.shape
 pixel_count = rows * cols
+
+# With sampling rate find the amount of sampled pixels
 sampled_pixel_count = int(pixel_count * SAMPLING_RATIO)
 
-
+# build tensors
 def build_train_tensors():
-    img_mask_x = torch.from_numpy(
-        np.random.randint(0, rows, sampled_pixel_count))
-    img_mask_y = torch.from_numpy(
-        np.random.randint(0, cols, sampled_pixel_count))
+    img_mask_x = torch.from_numpy(np.random.randint(0, rows, sampled_pixel_count))
+    img_mask_y = torch.from_numpy(np.random.randint(0, cols, sampled_pixel_count))
 
     img_train = img_ground_truth[img_mask_x, img_mask_y]
 
@@ -52,11 +61,12 @@ train_dataset = TensorDataset(img_mask, img_train)
 train_dataloader = DataLoader(
     train_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
 
-# Build model
+# Using SIREN build the model
+# Model Parameters
 layers = [256, 256, 256, 256, 256]
 in_features = 2
-# Ben changed out_features from 3 to 4 don't know why but that fixed it lmao
 out_features = 4
+# Initialize with default siren intializer
 initializer = 'siren'
 w0 = 1.0
 w0_initial = 30.0
@@ -71,43 +81,46 @@ BATCH_SIZE = min(BATCH_SIZE, len(img_mask))
 num_steps = int(len(img_mask) * EPOCHS / BATCH_SIZE)
 print("Total training steps : ", num_steps)
 
-# TODO scheduler
-# learning_rate = tf.keras.optimizers.schedules.PolynomialDecay(
-#   0.0005, decay_steps=num_steps, end_learning_rate=5e-5, power=2.0)
-# TODO: tensorboard
-
-#learning rate decay rate
-decay_rate = 0.9999
-
+# If CUDA is supported use it
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Create the optimizer with the model parameters, and initial learning rate
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-#set up lr scheduler for exponential weight decay
-#learning_rate = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=decay_rate)
+
+# Apply a learning rate scheduler with exponential decay to the optimizer
+learning_rate = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=decay_rate)
+
 criterion = torch.nn.MSELoss()
 
+# Set directory to save model
 checkpoint_dir = 'checkpoints/siren/inpainting/'
 if not os.path.exists(checkpoint_dir):
     os.makedirs(checkpoint_dir)
 
+# Create logger
 timestamp = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 logdir = os.path.join('logs/siren/inpainting/', timestamp)
-
 if not os.path.exists(logdir):
     os.makedirs(logdir)
-
 set_logger(os.path.join(logdir, 'train.log'))
 
+# Initialize lists used for measuring loss improvements
 epoch_losses = []
 epoch_improvements = []
+
+# Initialize starting loss to infinite
 best_loss = np.inf
 
-before = datetime.now()
+# Save initial timestamp before training
+timestamp = datetime.now()
 
+# Loop over image
 for epoch in range(EPOCHS):
     iterator = tqdm(train_dataloader, dynamic_ncols=True)
 
     losses = []
-
+    
+    # Train for each batch
     for batch in iterator:
         inputs, targets = batch
         predictions = model(inputs)
@@ -124,27 +137,32 @@ for epoch in range(EPOCHS):
     avg_loss = torch.mean(torch.cat(losses)).item()
     epoch_losses.append([epoch, avg_loss])
 
-    #step lr scheduler
-    #learning_rate.step()
-    if avg_loss < best_loss:
-        logging.info('Loss improved from {:.4f} to {:.4f}'.format(
-            best_loss, avg_loss))
-        best_loss = avg_loss
-        epoch_improvements.append([epoch,best_loss])
-        torch.save(
-            {'network': model.state_dict()},
-            os.path.join(checkpoint_dir + 'model'))
+    # At the end of the epoch decay the learning rate
+    learning_rate.step()
 
+    # If improved loss, save the epoch number and loss
+    if avg_loss < best_loss:
+        logging.info('Loss improved from {:.4f} to {:.4f}'.format(best_loss, avg_loss))
+
+        best_loss = avg_loss
+        epoch_improvements.append([epoch, best_loss])
+
+        # Save the improved model
+        torch.save({'network': model.state_dict()}, os.path.join(checkpoint_dir + 'model'))
+
+# Save and print the total time model took to train
 complete = datetime.now()
-dif = complete - before
+dif = complete - timestamp
 before_str = before.strftime("%H:%M:%S")
 logging.info('Started at: ' + before_str)
 complete_str = complete.strftime("%H:%M:%S")
 logging.info('Completed at: ' + complete_str)
 logging.info('Total time: ' + str(dif.seconds//3600) + ':' + str((dif.seconds//60) % 60) + ":" + str(dif.seconds%60))
 
+# Save a copy of the model to the saved models dir
 torch.save(model.state_dict(), "saved models/"+filename+" model.pt")
 
+# Write the epoch losses to csv files
 with open('metrics/'+filename+' - avg losses.csv','w+') as losses_file:
     writer = csv.writer(losses_file)
     writer.writerows(epoch_losses)
@@ -153,6 +171,7 @@ with open('metrics/'+filename+' - improvements.csv','w+') as improvements_file:
     writer = csv.writer(improvements_file)
     writer.writerows(epoch_improvements)
 
+# Save and plot the epoch losses
 epoch_losses = np.array(epoch_losses)
 plt.figure(0)
 x,y = epoch_losses.T
